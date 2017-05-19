3675e410b1bf31a3f20ec68842ab1bd113d879a2
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-18 23:27:21 -0400

    same as last-  forgot to save b4 last commit

--- research_documentation.rb
+++ research_documentation.rb
@@ -2,55 +2,75 @@
 
 # Don't just STRING me along
 
-*** length
+# *** length
+#
+# Arguments
+#       - NONE
+#
+# Returns
+#       - numerical length of characters in a string
+#       - can be called on other obejects such as Arrays
+#
+# Code
+#       "Hello".length #=> 5
+#       var = "howdy"
+#       var.length #=> 5
+#
+#
+# *** strip
+#
+# Arguments
+#       - NONE
+#
+# Returns
+#       - copy of string w/out whitespaces(ie. pretty much anything that isn't the string text itself)
+#
+# Code
+#       "    nowhitespace    ".strip #=> "nowhitespace"
+#
+#
+# *** split
+#
+# Arguments
+#       - 2 optional
+#         1st: pattern(defaults to nil)
+#         2nd: limit
+#
+# Returns
+#       - an Array
+#
+# Code
+#     "What does split do?".split #=> ["What", "does", "split", "do?"]
+#     "What does split do?".split(" ",2) #=> ["What", "does split do?"]
+#
+#
+# *** start_with?
+#
+# Arguments
+#       - 1 required
+#         (a prefix)
+#
+# Returns
+#       - boolean
+#
+# Code
+#     "discombobulated".start_with?("disco") #=> true
+
+# ARRAY of light
+
+*** first
 
 Arguments
-      - NONE
+      - 1 Optional (n) = number of elements to return
 
 Returns
-      - numerical length of characters in a string
-      - can be called on other obejects such as Arrays
+      - The First or First "n" elements of the array
 
 Code
-      "Hello".length #=> 5
-      var = "howdy"
-      var.length #=> 5
+  array = ["What", "does", "first", "do?"]
+  array.first #=> "What"
+
+  array.first(3) #=> ["What", "does", "first"]
 
 
-*** strip
-
-Arguments
-      - NONE
-
-Returns
-      - copy of string w/out whitespaces(ie. pretty much anything that isn't the string text itself)
-
-Code
-      "    nowhitespace    ".strip #=> "nowhitespace"
-
-
-*** split
-
-Arguments
-      - 2 optional
-        1st: pattern(defaults to nil)
-        2nd: limit
-
-Returns
-      - an Array
-
-Code
-    "What does split do?".split #=> ["What", "does", "split", "do?"]
-    "What does split do?".split(" ",2) #=> ["What", "does split do?"]
-
-
-*** start_with?
-
-Arguments
-      - 1 required
-        (a prefix)
-
-Returns
-      - boolean
-
-Code
+*** 
